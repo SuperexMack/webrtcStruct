ff05cd72c26c8e1201e0c7eabe7906160c5c6cd7
SuperexMack <unilexerox@gmail.com>
2026-02-15 18:07:24 +0530

    This is the backend part

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,30 @@ let senderSide = null,recSide = null
 
 wss.on("connection",(ws)=>{
     ws.on("message",(data)=>{
-       let message = JSON.stringify(data);
-       if(message.data === "SENDER"){
+       let message = JSON.parse(data);
+       if(message.msg === "SENDER"){
         senderSide = ws
        }
-       else if(message.data === "REMOTE"){
+       else if(message.msg === "REMOTE"){
         recSide = ws
        }
-       
+       else if(message.msg === "ice-candidate"){
+          if(ws === senderSide){
+            senderSide.send(JSON.stringify({msg:"ice-candidate",candidate:message.candidate}))
+          }
+          else{
+            recSide.send(JSON.stringify({msg:"ice-candidate",candidate:message.candidate}))
+          }
+       }
+       else if(message.msg === "create-offer"){
+         if(ws !== senderSide) return
+         recSide.send(JSON.stringify({type : "create-offer" , sdp : message.sdp}))
+       }
+
+       else if(message.msg === "create-answer"){
+         if(ws !== recSide) return
+         senderSide.send(JSON.stringify({type : "create-answer" , sdp : message.sdp}))
+       }
+
     })
 })
